da1685b0af2e8cb61f2d2fcd79516f5b0a65455e
Alexander Karevo <karevo1@mail.ru>
2026-01-21 23:47:43 +0300

    Code first try

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControl : MonoBehaviour
+{
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+	{
+		Debug.Log("Hello, World!\n");
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Keyboard.current.spaceKey.wasPressedThisFrame){
+			Debug.Log("JUMP!\n");
+		}
+		;
+	}
+}
