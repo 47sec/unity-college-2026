c611296112123bf73c0f92002c009fd77b2193fc
Alexander Karevo <karevo1@mail.ru>
2026-01-30 02:32:14 +0300

    Prepare 30_01_26
    Nigth home prepare to lesson. Need to sleep

---
+++ Assets/Scripts/PlayerControl1.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerControl1 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Это Карево А.С. и это мой любимый куб на Цитадели");//Данная команда сработает перед первым кадром игры 1 раз
+        //Команда позволяет вывести сообщение в консоль
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
